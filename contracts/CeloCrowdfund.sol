ae3be9b2dc2a28b5fe248bcaebd9106e6ba96263
Alex Reyes <reyesruizalex@gmail.com>
2021-04-09 17:37:07 -0400

    Semi working interact, working contract

--- contracts/CeloCrowdfund.sol
+++ contracts/CeloCrowdfund.sol
@@ -3,12 +3,14 @@ pragma solidity >=0.4.22 <0.9.0;
 
 // Importing OpenZeppelin's SafeMath Implementation
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CeloCrowdfund {
   using SafeMath for uint256; 
 
   // List all the projects 
   Project[] private projects; 
+  IERC20 private _cUSDToken;
 
   // event for when new project starts
   event ProjectStarted(
@@ -21,7 +23,18 @@ contract CeloCrowdfund {
     uint256 goalAmount
   ); 
 
+  constructor(IERC20 token) {
+    _cUSDToken = token; 
+  }
+
+  function testTokenTransfer(uint256 amount) external {
+    address from = msg.sender;
+
+    _cUSDToken.transferFrom(from, address(this), amount);
+  }
+
   function startProject(
+    IERC20 _cUSDToken,
     string calldata title,
     string calldata description,
     string calldata imageLink, 
@@ -29,7 +42,7 @@ contract CeloCrowdfund {
     uint amountToRaise
   ) external {
     uint raiseUntil = block.timestamp.add(durationInDays.mul(1 days)); 
-    Project newProject = new Project (payable(msg.sender), title, description, imageLink, raiseUntil, amountToRaise); 
+    Project newProject = new Project (_cUSDToken, payable(msg.sender), title, description, imageLink, raiseUntil, amountToRaise); 
     projects.push(newProject); 
     emit ProjectStarted(
       address(newProject),
@@ -55,6 +68,7 @@ contract Project {
     Expired, 
     Successful
   }
+  IERC20 private _cUSDToken;
 
   address payable public creator; 
   uint public goalAmount; 
@@ -81,6 +95,7 @@ contract Project {
 
   constructor
   (
+    IERC20 token,
     address payable projectCreator, 
     string memory projectTitle,
     string memory projectDescription,
@@ -88,6 +103,7 @@ contract Project {
     uint fundRaisingDeadline,
     uint projectGoalAmount
   ) {
+    _cUSDToken = token; 
     creator = projectCreator; 
     title = projectTitle; 
     description = projectDescription;
@@ -98,38 +114,41 @@ contract Project {
   }
 
   // Fund a certain project
-  function contribute() external theState(ProjectState.Fundraising) payable {
+  function contribute(uint256 amount) external theState(ProjectState.Fundraising) payable {
     // require(msg.sender != creator);
-    contributions[msg.sender] = contributions[msg.sender].add(msg.value);
-    currentBalance = currentBalance.add(msg.value);
-    emit ReceivedFunding(msg.sender, msg.value, currentBalance);
+    _cUSDToken.transferFrom(msg.sender, address(this), amount);
+
+    contributions[msg.sender] = contributions[msg.sender].add(amount);
+    currentBalance = currentBalance.add(amount);
+    emit ReceivedFunding(msg.sender, amount, currentBalance);
     // checkIfFundingCompleteOrExpired();
   }
 
   // check project state
   function checkIfFundingCompleteOrExpired() public {
-    if (currentBalance >= goalAmount) {
-      state = ProjectState.Successful; 
-      payOut(); 
-    } else if (block.timestamp > raisingDeadline) {
+    if (block.timestamp > raisingDeadline) {
       state = ProjectState.Expired;
     }
     completeAt = block.timestamp; 
   }
 
-  function payOut() internal theState(ProjectState.Successful) returns (bool) {
+  function payOut() external returns (bool) {
+    // require(msg.sender == creator); 
+
     uint256 totalRaised = currentBalance; 
     currentBalance = 0; 
 
-    if (creator.send(totalRaised)) {
-      emit CreatorPaid(creator);
-      return true; 
-    } else { 
-      currentBalance = totalRaised; 
-      state = ProjectState.Successful;
-    }
+    _cUSDToken.transfer(msg.sender, totalRaised);
 
-    return false; 
+    // if (_cUSDToken.transfer(msg.sender, totalRaised)) {
+    //   emit CreatorPaid(creator);
+    //   return true; 
+    // } else { 
+    //   currentBalance = totalRaised; 
+    //   state = ProjectState.Successful;
+    // }
+
+    // return false; 
   }
 
   function getDetails() public view returns 
--- scripts/interact.js
+++ scripts/interact.js
@@ -4,6 +4,8 @@ const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
 const kit = ContractKit.newKitFromWeb3(web3)
 const CeloCrowdfund = require('../build/contracts/CeloCrowdfund.json');
 const Project = require('../build/contracts/Project.json')
+const BigNumber = require('bignumber.js');
+
 require('dotenv').config({path: '../.env'});
 
 var projectData = []; 
@@ -15,59 +17,90 @@ async function initContract(){
   // Get the contract associated with the current network
   const deployedNetwork = CeloCrowdfund.networks[networkId]
 
-  // Create a new contract instance with the HelloWorld contract info
+  // Create a new contract instance from the celo crowdfund contract
   let celoCrowdfundContract = new kit.web3.eth.Contract(
       CeloCrowdfund.abi,
       deployedNetwork && deployedNetwork.address
   )
+
   var projectInstanceContract = ''; 
 
-  // Return results inside each individual project
+  // Return projects inside the celo crowdfund contract
   var result = await celoCrowdfundContract.methods.returnProjects().call();
 
-  /* Note: For some reason using forEach is asynchronous, but for...of  
-      maintains the synchronous results of using await. 
-  */
+  // Loop through the existing projects
   for (const projectAddress of result) {  
+    // Create a new project instance for each project
     projectInstanceContract = new web3.eth.Contract(
       Project.abi,
       deployedNetwork && projectAddress
     );
-
+    
+    // Get the details for each project, and save it to projectData[]
     await projectInstanceContract.methods.getDetails().call().then((result) => {
       projectData.push({result: result, projectInstanceContract: projectInstanceContract});
     });
   }
 
+  // Get Celo account info 
   const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
   kit.connection.addAccount(account.privateKey)
 
   console.log("ADDRESS: ", account.address);
 
+  // Get the cUSD ContractKit wrapper 
   const stableToken = await kit.contracts.getStableToken();
+
+  var projectGoal = BigNumber(1E18);
+
+  // Shorthand function to pause script execution 
+  const wait = ms => new Promise(res => setTimeout(res, ms));
+
+  // Create a new project
+  await celoCrowdfundContract.methods.startProject(
+    stableToken.address, 'Test project title', 'test project description', 'https://i.imgur.com/T9RAp1T.jpg', 5, projectGoal)
+    .send({from: account.address, feeCurrency: stableToken.address})
+    .then(async () => {
+      console.log("Created a new project"); 
+
+      var projectDetails = await projectInstanceContract.methods.getDetails().call();
+      console.log("PROJECT DETAILS: ", projectDetails);
+      console.log("Contract address: ", projectInstanceContract._address);
+
+      // Celo uses 18 decimal places. Set approval to be for 5 cUSD 
+      var approvedAmount = BigNumber(5E18); 
+      
+      // Need to approve the use of using cUSD with the smart contract. 
+      await stableToken.approve(projectInstanceContract._address, approvedAmount).send({from: account.address, feeCurrency: stableToken.address});
+      
+      console.log("Approved spending for new project");
+
+      /* We need to wait for the approval to be mined and reflected by the Celo network. 
+         To keep things simple we can just wait 7 seconds [not recommended for non-tutorial scripts] */
+      await wait(7000); 
+      console.log("Done waiting 7 seconds");
+    }).then(async () => {
+      // Send 2 cUSD to the contract
+      var sendAmount = BigNumber(2E18); 
+
+      // call contribute() function with 2 cUSD
+      await projectInstanceContract.methods.contribute(sendAmount).send({from: account.address, feeCurrency: stableToken.address});
+      console.log("Donated to new project");
+    }).then(async () => {
+      var balanceOfContract = (await stableToken.balanceOf(projectInstanceContract._address)).toString();
+      console.log("Contract address: ", projectInstanceContract._address);
+      console.log("Contract cUSD balance: ", balanceOfContract);
+    
+      var balanceOfUser = (await stableToken.balanceOf(account.address)).toString();
+      console.log("User's address: ", account.address);
+      console.log("User's cUSD balance: ", balanceOfUser);
+    });
  
-  // const txObject = await celoCrowdfundContract.methods.startProject(stableToken.address, 'test title', 'test desc', 'https://i.imgur.com/15SD9vT.jpeg', 5, 10).send({from: account.address, feeCurrency: stableToken.address});
-  // console.log("Made new project"); 
 
-  const approveTx = await stableToken.approve(projectInstanceContract._address, 10000000).send({from: account.address, feeCurrency: stableToken.address});
-  console.log("Approved spending for new project");
 
-  // var donate = await projectInstanceContract.methods.contribute(11).send({from: account.address, feeCurrency: stableToken.address});
-  // console.log("Donated to new project");
+  // var payOut = await projectInstanceContract.methods.payOut().send({from: account.address, feeCurrency: stableToken.address});
+  // console.log("Paying out from project")
 
-  var payOut = await projectInstanceContract.methods.payOut().send({from: account.address, feeCurrency: stableToken.address});
-  console.log("Paying out from project")
-
-  var projectDetails = await projectInstanceContract.methods.getDetails().call();
-  console.log("PROJECT DETAILS: ", projectDetails);
-  
-  var balanceOfContract = (await stableToken.balanceOf(projectInstanceContract._address)).toString();
-  console.log("Contract address: ", projectInstanceContract._address);
-  console.log("Contract cUSD balance: ", balanceOfContract);
-
-  var balanceOfUser = (await stableToken.balanceOf(account.address)).toString();
-  console.log("User's address: ", account.address);
-  console.log("User's cUSD balance: ", balanceOfUser);
 }
 
 initContract();
